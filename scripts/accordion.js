1fb3814808537796111481f1f88a037289c48342
yue lin(Christine) <liny498@newschool.edu>
2016-10-10 10:21:44 -0400

    update

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -7,3 +7,35 @@
 //         contentSelector: c
 //     })
 // }
+(function(){
+    //loading all the contents and let you ready to use them
+    if(document.readyState != "loading"){
+        handleAccordion();
+    }else{
+        document.addEventListener("DOMContentLoaded", function(){
+            handleAccordion();
+        })
+    }
+})();
+
+
+function handleAccordion(){
+    let accordions = document.getElementsByClassName("accordion");
+    for(accordion of accordions){
+        let navis = accordion.getElementsByClassName("acc-navi");
+        for(navi of navis){
+
+            navi.addEventListener("click", function(e){
+                this.classList.toggle('active');
+                // this.nextElementChild.classList('active');
+                let titles = accordion.getElementsByClassName("title");
+                for(title of titles){
+                    title.classList.toggle('active');
+                    //what's the difference between toggle vs toggleclass
+                    //when to use this.classList, when to use element.classList
+                    //how to deal with the side scroll bar?
+                }
+            });
+        }
+    }
+}
---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+(function() {
+    let selectors = {
+        naviSelector: '.acc-navi',
+        rootSelector: '.accordion',
+        contentSelector: '.content',
+        titleSelector: '.title',
+        titleIconSelector:'.title-expand'
+
+    };
+    let selectors2 = {
+        naviSelector: '.acc-sub-navi',
+        rootSelector: '.accordion-2',
+        contentSelector: '.content',
+        titleSelector: '.title',
+        titleIconSelector:'.title-expand'
+    };
+    if (document.readyState != 'loading') {
+        let accordions = new SimpleAccordion(selectors, { enabled: true, openingDuration: 200, closingDuration: 200, resolution: 5, mutualExclusive: false });
+        let accordions2 = new SimpleAccordion(selectors2, { enabled: true, openingDuration: 200, closingDuration: 200, resolution: 5 });
+    } else {
+        document.addEventListener('DOMContentLoaded', () => {
+            let accordions = new SimpleAccordion(selectors, { enabled: true, openingDuration: 200, closingDuration: 200, resolution: 5 });
+            let accordions2 = new SimpleAccordion(selectors2, { enabled: true, openingDuration: 200, closingDuration: 200, resolution: 5 });
+        });
+    }
+})();
